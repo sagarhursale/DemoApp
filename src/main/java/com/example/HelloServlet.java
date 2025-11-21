c2a9ae93fa10dc543b34f59990ae720841cdb451
sagarhursale <rsagar.hursale143@gmail.com>
2025-11-21 08:53:59 +0530

    Update HelloServlet.java

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -14,6 +14,6 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        response.getWriter().println("<h1>Hii from admin</h1>");
+        response.getWriter().println("<h1>Hii from sagar</h1>");
     }
 }
